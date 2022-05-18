f708e23ef61dccd52dd6cb688055907078226f75
m-ghiringhelli <mghiring@gmail.com>
2022-05-18 16:41:16 -0700

    checks if logged in user matches user who created post

--- src/components/Requests/RequestItem.jsx
+++ src/components/Requests/RequestItem.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
+import { getUser } from '../../services/user';
 
 export default function RequestItem({ request }) {
-  const { id, title } = request;
-  console.log(request);
+  const { email } = getUser();
+  console.log('email', email);
+  const { id, title, user_email } = request;
+  const isRequestOwner = email === user_email;
   return (
     <div>
       <Link to={`/requests/${id}`}>
         {title}
       </Link>
+      <span>{user_email}</span>
     </div>
   )
 }
