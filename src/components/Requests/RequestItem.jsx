06e2c7137b31a05f544d470ee9e7a3471693af3a
m-ghiringhelli <mghiring@gmail.com>
2022-05-20 16:18:52 -0700

    removed code i moved over to requestdetail from requestitem

--- src/components/Requests/RequestItem.jsx
+++ src/components/Requests/RequestItem.jsx
@@ -4,10 +4,8 @@ import { useAuth } from '../../hooks/useAuth';
 import { getUser } from '../../services/user';
 
 export default function RequestItem({ request }) {
-  const { email } = getUser();
   const { id, title } = request;
   const user_email = request.email;
-  const isRequestCreator = email === user_email;
 
   return (
     <div>
@@ -15,13 +13,6 @@ export default function RequestItem({ request }) {
         {title}
       </Link>
       <p>{user_email}</p>
-      {isRequestCreator && (
-        <button>
-          <Link to={`/requests/edit/${id}`}>
-            edit
-          </Link>
-        </button>
-      )}
     </div>
   )
 }
