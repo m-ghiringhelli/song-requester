4cde22ea591702a7135e9ed3e4f0aea2ebb79e66
m-ghiringhelli <mghiring@gmail.com>
2022-05-20 16:19:05 -0700

    oops forgot something

--- src/components/Requests/RequestItem.jsx
+++ src/components/Requests/RequestItem.jsx
@@ -1,7 +1,5 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
-import { useAuth } from '../../hooks/useAuth';
-import { getUser } from '../../services/user';
 
 export default function RequestItem({ request }) {
   const { id, title } = request;
